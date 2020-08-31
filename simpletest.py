a76e049a15638056667ab97d0efddafce2b14901
patrickisroman <patrickec246@gmail.com>
2020-08-30 22:13:32 -0700

    Update preview

--- simpletest.py
+++ simpletest.py
@@ -8,22 +8,25 @@ from tensorflow.keras.layers import Conv2D, Input, BatchNormalization, ReLU, Max
 
 from u2net import U2NET
 
+adam = keras.optimizers.Adam(learning_rate=0.001, beta_1=.9, beta_2=.999, epsilon=1e-08)
 default_shape = (320, 320, 3)
 
 img = cv2.imread('skateboard.jpg')
 img = cv2.resize(img, dsize=default_shape[:2], interpolation=cv2.INTER_CUBIC)
+inp = np.expand_dims(img, axis=0)
 
 inputs = keras.Input(shape=default_shape)
-u2net = U2NET()
-outputs = u2net(inputs)
-model = keras.Model(inputs=inputs, outputs=outputs, name='u2netmodel')
+net    = U2NET()
+out    = net(inputs)
+model  = keras.Model(inputs=inputs, outputs=out, name='u2netmodel')
 
-model.compile(optimizer='sgd', loss='sparse_categorical_crossentropy', metrics=None)
+model.compile(optimizer=adam, loss='binary_crossentropy', metrics=None)
 model.summary()
-inp = np.expand_dims(img, axis=0)
-d1, d2, d3, d4, d5, d6, d7 = model.predict(inp)
 
+d1, d2, d3, d4, d5, d6, d7 = model.predict(inp)
 pred = d1[0,:,:,:]
+
+# plot input/output
 
 fig=plt.figure(figsize=(1, 2))
 fig.add_subplot(1, 2, 1)
